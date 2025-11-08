8866a021c36d73fa7648999f5363e88170569498
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-11-07 20:47:25 -0500

    Simplify downloading. Add extraction

--- lib/lib.py
+++ lib/lib.py
@@ -1,8 +1,6 @@
 import requests
 import os
 
-_base_dir = "data"
-
 
 def download_file(url, destination_path, force=False):
   """
@@ -12,19 +10,15 @@ def download_file(url, destination_path, force=False):
         url (str): The URL of the file to download.
         destination_path (str): The local path where the file will be saved.
     """
-  if not os.path.exists(_base_dir):
-    print(f"Creating downloads directory: {_base_dir}")
-    os.makedirs(_base_dir)
-  dest_path = os.path.join(_base_dir, destination_path)
-  if os.path.exists(dest_path):
+  if os.path.exists(destination_path):
     if not force:
       print(
-          f"Skip downloading {url} to {destination_path} because {dest_path} already exists."
+          f"Skip downloading {url} to {destination_path} because it already exists."
       )
       return
-    print(f"Force downloading {url} to {destination_path} at {dest_path}.")
+    print(f"Force downloading {url} to {destination_path}.")
   else:
-    print(f"Downloading {url} to {destination_path} at {dest_path}.")
+    print(f"Downloading {url} to {destination_path}.")
   try:
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -45,14 +39,14 @@ def download_file(url, destination_path, force=False):
       else:
         print(f"\rDownloaded {downloaded_bytes} bytes.", end="")
 
-    with open(dest_path, 'wb') as f:
+    with open(destination_path, 'wb') as f:
       for chunk in response.iter_content(chunk_size=8192):
         if chunk:  # Filter out keep-alive new chunks
           f.write(chunk)
           downloaded_bytes += len(chunk)
           _report()
     print(
-        f"File downloaded ({downloaded_bytes} bytes) successfully from {url} to {dest_path}"
+        f"File downloaded ({downloaded_bytes} bytes) successfully from {url} to {destination_path}"
     )
   except requests.exceptions.RequestException as e:
     print(f"Error downloading file: {e}")
